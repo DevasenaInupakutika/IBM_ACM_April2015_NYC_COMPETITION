3c17ab0a9e9e0291af883335b35a2652731476a9
DevasenaInupakutika <devasena.prasad@gmail.com>
2015-04-23 10:46:18 +0100

    Code for extracting tags from JSON file (that holds relative path to cached image)

---
+++ tag_extract_json.py
@@ -0,0 +1,30 @@
+import json
+import urllib
+import sys
+import os
+
+count = 0
+data = urllib.urlopen("http://degas.ecs.soton.ac.uk/~jsh2/nyc360/testroute.json").read()
+
+list_tags = []
+d = json.loads(data)
+
+for x in d['features']:
+    list_tags = x['properties']['tags']    
+   
+    for index in range(len(list_tags)):
+        # print type(list_tags[index])  # Data type of elements of each list list_tags i.e. dictionary 
+        print list_tags[index]['key']
+
+    # print type(list_tags)  # Data type of tags i.e. list
+    # count+=1
+
+    # if count == 3:
+    #    break
+
+#print list_tags[0]
+
+#from pprint import pprint
+
+#pprint(d['features'])
+
